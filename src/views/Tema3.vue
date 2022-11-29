836d8c75a7be56606a3e3c671f4643a6a434dbb0
Olimpo Macea <olimpo3m@gmail.com>
2022-11-28 22:35:30 -0500

    Video tema 3 vinvulado, pendiente itro

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -462,7 +462,7 @@
 
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/DboecE0kyYk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     .tarjeta.color-primario.p-3.mb-5
       .row.justify-content-around.align-items-center
